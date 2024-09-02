16e7c87d5eee65a4f3081ad25a19800ed2cacf94
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-09-01 23:54:26 -0400

    Arreglo completo de 6 elementos!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,8 @@
+let colores = ["Gris", "Blanco", "Rosa", "Amarillo", "Turquesa", "Tomate"];
+
+console.log(colores[0]);
+console.log(colores[1]);
+console.log(colores[2]);
+console.log(colores[3]);
+console.log(colores[4]);
+console.log(colores[5]);
